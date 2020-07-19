c0063ed0b9829c1d26d37a0c19a70aa35861af20
lucidck <curtiskilgore@hotmail.com>
2020-07-19 16:22:26 -0700

    Added CSS

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,9 +3,15 @@ import React from "react";
 export default function Search(props) {
   return (
     <div className="search">
-      <form onSubmit={props.searchBook} action="">
-        <input onChange={props.handleSearch} type="text" />
-        <button type="submit">Search</button>
+      <form onSubmit={props.searchBook}>
+        <input
+          className="search-box-input"
+          onChange={props.handleSearch}
+          type="text"
+        />
+        <button className="search-box-button" type="submit">
+          Search
+        </button>
         <select defaultValue="Sort" onChange={props.handleSort}>
           <option value="Sort">Sort</option>
           <option value="Newest">Newest</option>
